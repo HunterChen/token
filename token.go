c5fcc2c17454ec81616aa2f7cef6c60e39d32852
MamoruIzuka <m-izuka@jayb.jp>
2016-03-31 12:42:56 +0900

    golint

--- token.go
+++ token.go
@@ -13,26 +13,68 @@ import (
 
 const tokenDuration = 72
 
-type Token struct {
-	Id         string
-	Expiration int64
-	Signature  string
-}
-
-type TokenError struct {
+// Error is a model for token error
+type Error struct {
 	Message string
 	Details error
 }
 
+// Token is a model for token
+type Token struct {
+	ID         string
+	Expiration int64
+	Signature  string
+}
+
+// GetTokenString is to get token string
+func (t *Token) GetTokenString() string {
+	var parts [2]string
+
+	parts[0] = makeEncodeParams(t.ID, t.Expiration)
+	parts[1] = t.Signature
+
+	return strings.Join(parts[:], ".")
+}
+
+// IsValid is validates Token
+func (t *Token) IsValid(key *rsa.PublicKey) *Error {
+	signatureBytes, err := decode(t.Signature)
+	if err != nil {
+		return &Error{"Tokenが不正です", err}
+	}
+
+	now := time.Now().Unix()
+
+	if t.Expiration < now {
+		return &Error{"Tokenの期限が切れています", nil}
+	}
+
+	encodeParams := makeEncodeParams(t.ID, t.Expiration)
+
+	if !hash.Available() {
+		return &Error{"ハッシュの初期化に失敗しました", nil}
+	}
+
+	hs := hash.New()
+	hs.Write([]byte(encodeParams))
+
+	if err := rsa.VerifyPKCS1v15(key, hash, hs.Sum(nil), signatureBytes); err != nil {
+		return &Error{"シグネチャが不正です", err}
+	}
+
+	return nil
+}
+
 var hash = crypto.SHA256
 
-func CreateToken(key *rsa.PrivateKey, id string) (*Token, *TokenError) {
+// CreateToken is to create a token
+func CreateToken(key *rsa.PrivateKey, id string) (*Token, *Error) {
 	expiration := time.Now().Add(time.Hour * tokenDuration).Unix()
 
 	encodeParams := makeEncodeParams(id, expiration)
 
 	if !hash.Available() {
-		return nil, &TokenError{"ハッシュの初期化に失敗しました", nil}
+		return nil, &Error{"ハッシュの初期化に失敗しました", nil}
 	}
 
 	hs := hash.New()
@@ -40,22 +82,23 @@ func CreateToken(key *rsa.PrivateKey, id string) (*Token, *TokenError) {
 
 	signature, err := rsa.SignPKCS1v15(rand.Reader, key, hash, hs.Sum(nil))
 	if err != nil {
-		return nil, &TokenError{"シグネチャの生成に失敗しました", err}
+		return nil, &Error{"シグネチャの生成に失敗しました", err}
 	}
 
 	return &Token{id, expiration, encode(signature)}, nil
 }
 
-func ParseToken(tokenString string) (*Token, *TokenError) {
+// ParseToken is parse token string to token
+func ParseToken(tokenString string) (*Token, *Error) {
 	parts := strings.Split(tokenString, ".")
 
 	if len(parts) != 2 {
-		return nil, &TokenError{"Tokenが不正です", nil}
+		return nil, &Error{"Tokenが不正です", nil}
 	}
 
 	decodeParams, err := decode(parts[0])
 	if err != nil {
-		return nil, &TokenError{"パラメータのデコードに失敗しました", err}
+		return nil, &Error{"パラメータのデコードに失敗しました", err}
 	}
 
 	var params map[string]interface{}
@@ -63,62 +106,25 @@ func ParseToken(tokenString string) (*Token, *TokenError) {
 	decoder := json.NewDecoder(bytes.NewBuffer(decodeParams))
 	decoder.UseNumber()
 	if err := decoder.Decode(&params); err != nil {
-		return nil, &TokenError{"不明なエラー", err}
+		return nil, &Error{"不明なエラー", err}
 	}
 
 	id, ok := params["Id"].(string)
 	if !ok {
-		return nil, &TokenError{"Idが不正です", nil}
+		return nil, &Error{"Idが不正です", nil}
 	}
 
 	exNum, ok := params["Expiration"].(json.Number)
 	if !ok {
-		return nil, &TokenError{"Expirationが不正です", nil}
+		return nil, &Error{"Expirationが不正です", nil}
 	}
 
 	expiration, err := exNum.Int64()
 	if err != nil {
-		return nil, &TokenError{"不明なエラー", err}
+		return nil, &Error{"不明なエラー", err}
 	}
 
 	return &Token{id, expiration, parts[1]}, nil
-}
-
-func (t *Token) GetTokenString() string {
-	var parts [2]string
-
-	parts[0] = makeEncodeParams(t.Id, t.Expiration)
-	parts[1] = t.Signature
-
-	return strings.Join(parts[:], ".")
-}
-
-func (t *Token) IsValid(key *rsa.PublicKey) *TokenError {
-	signatureBytes, err := decode(t.Signature)
-	if err != nil {
-		return &TokenError{"Tokenが不正です", err}
-	}
-
-	now := time.Now().Unix()
-
-	if t.Expiration < now {
-		return &TokenError{"Tokenの期限が切れています", nil}
-	}
-
-	encodeParams := makeEncodeParams(t.Id, t.Expiration)
-
-	if !hash.Available() {
-		return &TokenError{"ハッシュの初期化に失敗しました", nil}
-	}
-
-	hs := hash.New()
-	hs.Write([]byte(encodeParams))
-
-	if err := rsa.VerifyPKCS1v15(key, hash, hs.Sum(nil), signatureBytes); err != nil {
-		return &TokenError{"シグネチャが不正です", err}
-	}
-
-	return nil
 }
 
 func makeEncodeParams(id string, expiration int64) string {
